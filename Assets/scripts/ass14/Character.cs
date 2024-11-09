81d9bddbcccbfdcc900ff76553d9f5f695909d1b
tawfeeqabusamra <tefo.w.a.s13@gmail.com>
2024-11-09 12:12:47 +0200

    ..

--- Assets/scripts/ass14/Character.cs
+++ Assets/scripts/ass14/Character.cs
@@ -7,6 +7,7 @@ public class Character
     private string Name;
     private int Health;
     
+    
     public Character(string Name , int Health){
         this.Name= Name;
         this.Health= Health;
@@ -16,6 +17,19 @@ public class Character
     {
         get;
         set;
+    }
+    public int health
+    {
+        get { return Health; }
+        set
+        {
+            if (value > 100)
+                health = 100; 
+            else if (value < 0)
+                health = 0;   
+            else
+                health = value;
+        }
     }
 
 }
